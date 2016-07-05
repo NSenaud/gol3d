5706dd3a9616a043246df7dc17e8e987effabff8
Nicolas Senaud <nicolas@senaud.fr>
2016-07-05 18:33:36 +0200

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 extern crate kiss3d;
 extern crate nalgebra as na;
+
+use log::LogLevel;
 
 use na::Vector3;
 use na::Point3;
@@ -7,7 +12,10 @@ use kiss3d::window::Window;
 use kiss3d::light::Light;
 
 fn main() {
-    let mut window = Window::new("Kiss3d: cube");
+    env_logger::init().unwrap();
+    info!("Launching Game of Life 3D…");
+
+    let mut window = Window::new("Game of Life 3D");
     let mut c1     = window.add_cube(1.0, 1.0, 1.0);
     let mut c2     = window.add_cube(1.0, 1.0, 1.0);
     let mut c3     = window.add_cube(1.0, 1.0, 1.0);
@@ -20,7 +28,7 @@ fn main() {
 
     let c2move = Vector3 { x:2.0, y:0.0, z:0.0 };
     let c3move = Vector3 { x:0.0, y:2.0, z:0.0 };
-    let crmove = Vector3 { x:0.0, y:0.0, z:2.0 };
+    let mut crmove = Vector3 { x:0.0, y:0.0, z:2.0 };
     c2.append_translation(&c2move);
     c3.append_translation(&c3move);
     cr.append_translation(&crmove);
