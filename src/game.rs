36decbad90210a62c39ae464f29a27802ba26bef
Nicolas Senaud <nicolas@senaud.fr>
2016-07-07 11:19:04 +0200

    Added a 4th dimension in the cube

--- src/game.rs
+++ src/game.rs
@@ -1,27 +1,31 @@
 use ndarray::prelude::*;
 
 struct Game {
-    world: OwnedArray<bool, (Ix, Ix, Ix)>,
+    size: usize,
+    world: OwnedArray<bool, (Ix, Ix, Ix, Ix)>,
 }
 
 trait Life {
     fn new(size: usize) -> Option<Game>;
     fn init(&mut self);
+    fn next(&mut self);
 }
 
 impl Life for Game {
     /// Create an empty game cube with a given edge `size` (minimum 3).
     ///
-    /// The cube is initialized empty (i.e. `false` everywhere).
+    /// The cube is initialized empty (i.e. `false` everywhere) and has a fourth
+    /// dimension for the time (used to compute the next turn).
     ///
     /// If the `size` parameter is less than 3, `None` is returned.
     fn new(size: usize) -> Option<Game> {
         if size < 3 { return None; }
 
         let mut game = Game {
+            size: size,
             world: OwnedArray::from_shape_vec(
-                       (size, size, size),
-                       vec![false; size * size * size]
+                       (size, size, size, 2),
+                       vec![false; size * size * size * 2]
                    ).unwrap()
         };
 
@@ -33,11 +37,15 @@ impl Life for Game {
         for x in 0..1 {
             for y in 0..1 {
                 for z in 0..1 {
-                    let mut cell = self.world.get_mut((x, y, z)).unwrap();
+                    let mut cell = self.world.get_mut((x, y, z, 0)).unwrap();
                     *cell = true;
                 }
             }
         }
+    }
+
+    fn next(&mut self) {
+
     }
 }
 
@@ -49,7 +57,7 @@ mod tests {
     fn test_it_can_create_game() {
         let game = Game::new(3).unwrap();
 
-        assert_eq!(&false, game.world.get((0, 0, 0)).unwrap());
+        assert_eq!(&false, game.world.get((0, 0, 0, 0)).unwrap());
     }
 
     #[test]
@@ -60,7 +68,8 @@ mod tests {
         for x in 0..1 {
             for y in 0..1 {
                 for z in 0..1 {
-                    assert_eq!(&true, game.world.get((x, y, z)).unwrap());
+                    assert_eq!(&true, game.world.get((x, y, z, 0)).unwrap());
+                    assert_eq!(&false, game.world.get((x, y, z, 1)).unwrap());
                 }
             }
         }
