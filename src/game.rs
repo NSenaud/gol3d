a4a8344f7d8067ca7fe0fc638fa15a25a7c9d5b7
Nicolas Senaud <nicolas@senaud.fr>
2018-01-03 05:27:24 +0100

    It works!
    * tests
    * increment board size
    * updates dependencies
    * requires optimizations

--- src/game.rs
+++ src/game.rs
@@ -1,14 +1,20 @@
 use ndarray::prelude::*;
 
-struct Game {
+#[derive(Debug, PartialEq)]
+pub struct Game {
     size: usize,
-    world: OwnedArray<bool, (Ix, Ix, Ix, Ix)>,
+    pub world: Array<usize, Ix4>,
 }
 
-trait Life {
+pub type Position = (usize, usize, usize);
+
+pub trait Life {
     fn new(size: usize) -> Option<Game>;
     fn init(&mut self);
     fn next(&mut self);
+    fn get_future_state(&self, x: usize, y: usize, z: usize) -> usize;
+    fn neighbours_of(&self, pos: Position) -> Vec<Position>;
+    fn get_range(&self, v: usize) -> Vec<usize>;
 }
 
 impl Life for Game {
@@ -21,43 +27,113 @@ impl Life for Game {
     fn new(size: usize) -> Option<Game> {
         if size < 3 { return None; }
 
-        let mut game = Game {
+        let game = Game {
             size: size,
-            world: OwnedArray::from_shape_vec(
-                       (size, size, size, 2),
-                       vec![false; size * size * size * 2]
-                   ).unwrap()
+            world: Array::<usize, Ix4>::zeros(Ix4(size, size, size, 2)),
         };
 
         Some(game)
     }
 
-    /// Initialize the cube with nine cells from (0, 0, 0) to (1, 1, 1)
+    /// Initialize the cube with eight cells from (0, 0, 0) to (1, 1, 1)
     fn init(&mut self) {
-        for x in 0..1 {
-            for y in 0..1 {
-                for z in 0..1 {
+        for x in 0..2 {
+            for y in 0..2 {
+                for z in 0..2 {
                     let mut cell = self.world.get_mut((x, y, z, 0)).unwrap();
-                    *cell = true;
+                    *cell = 1;
+                }
+            }
+        }
+
+        let mut cell = self.world.get_mut((2, 2, 2, 0)).unwrap();
+        *cell = 1;
+    }
+
+    fn next(&mut self) {
+        for x in 0..self.size {
+            for y in 0..self.size {
+                for z in 0..self.size {
+                    let mut state = self.get_future_state(x, y, z);
+                    let mut future_cell = self.world.get_mut((x, y, z, 1)).unwrap();
+                    *future_cell = state;
+
+                    debug!("({},{},{}) future state is {}", x, y, z, future_cell);
+                }
+            }
+        }
+
+        // Update current from future
+        for x in 0..self.size {
+            for y in 0..self.size {
+                for z in 0..self.size {
+                    let mut state = self.world.get((x, y, z, 1)).unwrap().clone();
+                    let mut past = self.world.get_mut((x, y, z, 0)).unwrap();
+                    *past = state;
+
+                    debug!("({},{},{}) new state is {}", x, y, z, past);
                 }
             }
         }
     }
 
-    fn next(&mut self) {
+    fn get_future_state(&self, x: usize, y: usize, z: usize) -> usize {
+        let mut count = 0;
 
+        for n in self.neighbours_of((x, y, z)) {
+            if *self.world.get((n.0, n.1, n.2, 0)).unwrap() == 1 {
+                debug!("{:?} is alive", n);
+                count += 1;
+            }
+        }
+
+        if count < 2 || count > 4 {
+            debug!("({},{},{}) has {} neighbours -> 0", x, y, z, count);
+            return 0
+        } else {
+            debug!("({},{},{}) has {} neighbours -> 1", x, y, z, count);
+            return 1
+        }
+    }
+
+    fn neighbours_of(&self, pos: Position) -> Vec<Position> {
+        let mut positions = Vec::<Position>::new();
+
+        for x in self.get_range(pos.0) {
+            for y in self.get_range(pos.1) {
+                for z in self.get_range(pos.2) {
+                    if (x, y, z) != pos && !positions.contains(&(x, y, z)) {
+                        positions.push((x, y, z));
+                    }
+                }
+            }
+        }
+
+        debug!("{:?} neighbours are: {:?}", pos, positions);
+        positions
+    }
+
+    fn get_range(&self, v: usize) -> Vec<usize> {
+        let mut range = Vec::new();
+
+        if v > 0 { range.push(v - 1) };
+        range.push(v);
+        if v + 1 < self.size { range.push(v + 1) };
+
+        range
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{ Game, Life };
+    use super::{Game, Life};
 
     #[test]
     fn test_it_can_create_game() {
         let game = Game::new(3).unwrap();
+        assert!(!game.world.is_empty());
 
-        assert_eq!(&false, game.world.get((0, 0, 0, 0)).unwrap());
+        assert_eq!(None, Game::new(2));
     }
 
     #[test]
@@ -65,13 +141,108 @@ mod tests {
         let mut game = Game::new(3).unwrap();
         game.init();
 
-        for x in 0..1 {
-            for y in 0..1 {
-                for z in 0..1 {
-                    assert_eq!(&true, game.world.get((x, y, z, 0)).unwrap());
-                    assert_eq!(&false, game.world.get((x, y, z, 1)).unwrap());
-                }
-            }
-        }
+        assert_eq!(1, *game.world.get((0, 0, 0, 0)).unwrap());
+        assert_eq!(1, *game.world.get((0, 0, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((0, 1, 0, 0)).unwrap());
+        assert_eq!(1, *game.world.get((0, 1, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 0, 0, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 0, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 1, 0, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 1, 1, 0)).unwrap());
+        assert_eq!(0, *game.world.get((1, 1, 2, 0)).unwrap());
+        assert_eq!(0, *game.world.get((1, 2, 1, 0)).unwrap());
+        assert_eq!(0, *game.world.get((1, 2, 2, 0)).unwrap());
+        assert_eq!(0, *game.world.get((2, 1, 1, 0)).unwrap());
+        assert_eq!(0, *game.world.get((2, 1, 2, 0)).unwrap());
+        assert_eq!(0, *game.world.get((2, 2, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((2, 2, 2, 0)).unwrap());
+    }
+
+    #[test]
+    fn test_neighbours_of() {
+        let mut game = Game::new(3).unwrap();
+        game.init();
+
+        assert_eq!(
+            vec![           (0, 0, 1),
+                 (0, 1, 0), (0, 1, 1),
+                 (1, 0, 0), (1, 0, 1),
+                 (1, 1, 0), (1, 1, 1)],
+            game.neighbours_of((0, 0, 0))
+        );
+        assert_eq!(
+            vec![(0, 0, 0),            (0, 0, 2),
+                 (0, 1, 0), (0, 1, 1), (0, 1, 2),
+                 (1, 0, 0), (1, 0, 1), (1, 0, 2),
+                 (1, 1, 0), (1, 1, 1), (1, 1, 2)],
+            game.neighbours_of((0, 0, 1))
+        );
+        assert_eq!(
+            vec![(0, 0, 0), (0, 0, 1), (0, 0, 2),
+                 (0, 1, 0), (0, 1, 1), (0, 1, 2),
+                 (0, 2, 0), (0, 2, 1), (0, 2, 2),
+                 (1, 0, 0), (1, 0, 1), (1, 0, 2),
+                 (1, 1, 0),            (1, 1, 2),
+                 (1, 2, 0), (1, 2, 1), (1, 2, 2),
+                 (2, 0, 0), (2, 0, 1), (2, 0, 2),
+                 (2, 1, 0), (2, 1, 1), (2, 1, 2),
+                 (2, 2, 0), (2, 2, 1), (2, 2, 2)],
+            game.neighbours_of((1, 1, 1))
+        );
+    }
+
+    #[test]
+    fn test_get_range() {
+        let mut game = Game::new(3).unwrap();
+        game.init();
+
+        assert_eq!(vec![0, 1], game.get_range(0));
+        assert_eq!(vec![0, 1, 2], game.get_range(1));
+        assert_eq!(vec![1, 2], game.get_range(2));
+    }
+
+    #[test]
+    fn test_get_future_state() {
+        let mut game = Game::new(3).unwrap();
+        game.init();
+
+        assert_eq!(1, game.get_future_state(0, 0, 0));
+        assert_eq!(1, game.get_future_state(0, 0, 1));
+        assert_eq!(1, game.get_future_state(0, 1, 0));
+        assert_eq!(1, game.get_future_state(0, 1, 1));
+        assert_eq!(1, game.get_future_state(1, 0, 0));
+        assert_eq!(1, game.get_future_state(1, 0, 1));
+        assert_eq!(1, game.get_future_state(1, 1, 0));
+        assert_eq!(0, game.get_future_state(1, 1, 1));
+        assert_eq!(1, game.get_future_state(1, 1, 2));
+        assert_eq!(1, game.get_future_state(1, 2, 1));
+        assert_eq!(1, game.get_future_state(1, 2, 2));
+        assert_eq!(1, game.get_future_state(2, 1, 1));
+        assert_eq!(1, game.get_future_state(2, 1, 2));
+        assert_eq!(1, game.get_future_state(2, 2, 1));
+        assert_eq!(0, game.get_future_state(2, 2, 2));
+    }
+
+    #[test]
+    fn test_it_compute_next_state() {
+        let mut game = Game::new(3).unwrap();
+        game.init();
+        game.next();
+
+        assert_eq!(1, *game.world.get((0, 0, 0, 0)).unwrap());
+        assert_eq!(1, *game.world.get((0, 0, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((0, 1, 0, 0)).unwrap());
+        assert_eq!(1, *game.world.get((0, 1, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 0, 0, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 0, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 1, 0, 0)).unwrap());
+        assert_eq!(0, *game.world.get((1, 1, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 1, 2, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 2, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((1, 2, 2, 0)).unwrap());
+        assert_eq!(1, *game.world.get((2, 1, 1, 0)).unwrap());
+        assert_eq!(1, *game.world.get((2, 1, 2, 0)).unwrap());
+        assert_eq!(1, *game.world.get((2, 2, 1, 0)).unwrap());
+        assert_eq!(0, *game.world.get((2, 2, 2, 0)).unwrap());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,115 @@ extern crate ndarray;
 
 mod game;
 
-use log::LogLevel;
+use std::{thread, time};
 
-use na::Vector3;
-use na::Point3;
+use na::{Vector3, Translation};
 use kiss3d::window::Window;
 use kiss3d::light::Light;
+
+use game::{Game, Life, Position};
+
+
+struct Drew {
+    cubes: Vec<(Position, kiss3d::scene::SceneNode)>,
+}
+
+impl Drew {
+    fn new() -> Drew {
+        Drew { cubes: Vec::new() }
+    }
+
+    fn save(&mut self, pos: Position, cube: kiss3d::scene::SceneNode) {
+        self.cubes.push((pos, cube))
+    }
+
+    fn remove(&mut self, index: usize) {
+        self.cubes.remove(index);
+    }
+
+    fn is_empty(&self) -> bool {
+        self.cubes.is_empty()
+    }
+
+    fn len(&self) -> usize {
+        self.cubes.len()
+    }
+}
+
 
 fn main() {
     env_logger::init().unwrap();
     info!("Launching Game of Life 3D…");
 
+    let size = 30;
+
+    let mut game = Game::new(size).unwrap();
+    game.init();
+
     let mut window = Window::new("Game of Life 3D");
-    let mut c1     = window.add_cube(1.0, 1.0, 1.0);
-    let mut c2     = window.add_cube(1.0, 1.0, 1.0);
-    let mut c3     = window.add_cube(1.0, 1.0, 1.0);
-    let mut cr     = window.add_sphere(0.5);
-
-    c1.set_color(1.0, 0.0, 0.0);
-    c2.set_color(0.0, 1.0, 0.0);
-    c3.set_color(0.0, 0.0, 1.0);
-    cr.set_color(1.0, 0.0, 1.0);
-
-    let c2move = Vector3 { x:2.0, y:0.0, z:0.0 };
-    let c3move = Vector3 { x:0.0, y:2.0, z:0.0 };
-    let mut crmove = Vector3 { x:0.0, y:0.0, z:2.0 };
-    c2.append_translation(&c2move);
-    c3.append_translation(&c3move);
-    cr.append_translation(&crmove);
-
     window.set_light(Light::StickToCamera);
 
+    render(&mut window, game, size);
+}
+
+// TODO: Parallelize game and rendering
+fn render<'a>(window: &'a mut Window, mut game: Game, size: usize) {
+    let mut cubes = Drew::new();
+
     while window.render() {
-        c1.prepend_to_local_rotation(&Vector3::new(0.0f32, 0.014, 0.0));
-        c2.prepend_to_local_rotation(&Vector3::new(0.0f32, -0.014, 0.0));
-        c3.prepend_to_local_rotation(&Vector3::new(0.0f32, 0.0, 0.014));
+        for x in 0..size {
+            for y in 0..size {
+                for z in 0..size {
+                    let alive = game.world.get((x, y, z, 0)).unwrap();
+
+                    // Cell is alive
+                    if alive != &0 {
+                        let mut already_alive = false;
+                        for cube in &cubes.cubes {
+                            if cube.0 == (x, y, z) {
+                                debug!("Cell already alive");
+                                already_alive = true;
+                                break;
+                            }
+                        }
+
+                        if !already_alive {
+                            debug!("Draw cell at {}, {}, {}", x, y, z);
+                            let mut c = window.add_cube(0.7, 0.7, 0.7);
+                            c.set_color(1., 0., 0.);
+                            let cmove = Vector3::new(x as f32, y as f32, z as f32);
+                            c.append_translation(&Translation {vector: cmove} );
+
+                            cubes.save((x, y, z), c);
+                        }
+                    } else {
+                        let mut index = None;
+                        for i in 0..cubes.len() {
+                            if cubes.cubes[i].0 == (x, y, z) {
+                                index = Some(i);
+                                break;
+                            }
+
+                        }
+                        
+                        match index {
+                            Some(index) => {
+                                window.remove(&mut cubes.cubes[index].1);
+                                cubes.remove(index);
+                            },
+                            None => (),
+                        }
+                    }
+                }
+            }
+        }
+
+        if cubes.is_empty() {
+            info!("This is the end!");
+            break;
+        }
+
+        game.next();
+        thread::sleep(time::Duration::from_millis(16));
     }
 }
